b27eae2a7c7f9d31555b331ddac74d532c9ef988
Dmitriy <cccrazy203@gmail.com>
2024-10-29 22:21:10 +0600

    Добавил визуализацию, Добавлена функция обратное число числу А

--- lb2/main.py
+++ lb2/main.py
@@ -1,9 +1,49 @@
-from list_def import modular_inverse
+from list_def import modular_inverse, extended_euclidean
 
-if __name__ == "__main__":
+def nod():
+    element = int(input("Введите элемент 'a': "))
+    modulus = int(input("Введите модуль 'm': "))
+
+    gcd, a, b = extended_euclidean(element, modulus)
+
+    if (a==0):
+        print(f"Достигнут базовый случай: a = 0, возвращаем (НОД={b}, x=0, y=1)")
+    else:
+        print(f"\nРезультат: НОД({a}, {b}) = {gcd}, коэффициенты x = {a}, y = {b}")
+
+def Obratno_A():
     # Ввод элемента и модуля с консоли
-    element = int(input("Введите элемент: "))
-    modulus = int(input("Введите модуль: "))
+    element = int(input("Введите элемент 'a': "))
+    modulus = int(input("Введите модуль 'm': "))
     
     # Вычисление и вывод результата
     modular_inverse(element, modulus)
+
+if __name__ == "__main__":
+
+    com = ''
+
+    while(com!='exit'):
+        print("1. Нахождения НОД(a,m)" +
+              "2. Вычисление элемента, обратного к данному элементу в кольце вычетов по заданному модулю.\n" +
+              "3. Решение сравнения вида 'ax mod m ≡ b'.\n" +
+              "4. Расшифровка.\n" +
+              "5. Шифровка\n" +
+              "exit\n")
+        
+        com = str(input("Выберите действие: "))
+
+        if (com == "1"):
+            nod()
+
+        elif (com == "2"):
+            Obratno_A()
+
+        elif (com == "3"):
+            pass
+
+        elif (com == "4"):
+            pass
+
+        elif (com == "5"):
+            pass
