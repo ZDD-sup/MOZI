2c68c387d4cfe08ff0ee0a586bcc16085a8c07ee
Dmitriy <cccrazy203@gmail.com>
2024-11-13 16:28:31 +0600

    Сделал расчёт энтропии

---
+++ lb3/list_def.py
@@ -0,0 +1,39 @@
+import re
+import math
+from collections import Counter
+
+def preprocess_text(text, alphabet):
+    text = text.lower()
+    text = text.replace('ё', 'е') if 'е' in alphabet else text
+    allowed_chars = f"[^{alphabet}]"
+    text = re.sub(allowed_chars, '', text)
+    return text
+
+# Функция для получения k-грамм
+def get_k_grams(text, k):
+    return [text[i:i+k] for i in range(len(text) - k + 1)]
+
+# Функция для вычисления частоты k-грамм
+def compute_frequencies(k_grams):
+    return Counter(k_grams)
+
+# Функция для вычисления энтропии k-грамм
+def compute_entropy(frequencies, total_k_grams):
+    entropy = 0
+    for count in frequencies.values():
+        p = count / total_k_grams
+        entropy -= p * math.log2(p)
+    return entropy
+
+# Функция для вычисления энтропии для разных значений k
+def calculate_entropy_for_k(text, max_k):
+    entropies = []
+    for k in range(1, max_k + 1):
+        k_grams = get_k_grams(text, k)
+        frequencies = compute_frequencies(k_grams)
+        total_k_grams = len(k_grams)
+        entropy = compute_entropy(frequencies, total_k_grams)
+        # Нормализуем энтропию
+        normalized_entropy = entropy / (k ** k)
+        entropies.append(normalized_entropy)
+    return entropies
---
+++ lb3/main.py
@@ -0,0 +1,19 @@
+from list_def import preprocess_text, calculate_entropy_for_k
+
+def main():
+    alphabet = "абвгдежзийклмнопрстуфхцчшщъыьэюя"  # 0-31 позиций, 32 шт
+    # Текст для анализа
+    text = str(input("Введите текст: "))
+
+    # Предобработка текста
+    processed_text = preprocess_text(text,alphabet)
+
+    # Расчет энтропии для k от 1 до 5
+    entropies = calculate_entropy_for_k(processed_text, 5)
+
+    # Вывод результатов
+    for k, entropy in enumerate(entropies, start=1):
+        print(f"Энтропия для {k}-грамм / {k}^{k}: {entropy:.4f}")
+
+if __name__ == "__main__":
+    main()
